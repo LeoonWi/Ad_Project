9e26b279aadd3e4f28409c4cf0d988a52439b66f
LeoonWi <redwind090@gmail.com>
2023-06-08 22:56:54 +0300

    Верстка списка заказов

--- frontend/src/views/User/OrdersView.vue
+++ frontend/src/views/User/OrdersView.vue
@@ -1,9 +1,25 @@
 <template>
-    <v-container>
-      <v-row justify="center">
-            <h1>OrderView</h1>
-      </v-row>
-    </v-container>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3 mt-3">Orders</h1>
+        <v-list subheader two-line flat>
+          <v-list-item v-for="order in orders" :key="order.id">
+            <v-list-item-action>
+              <v-checkbox :input-value="order.done" color="primary" />
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ order.name }}</v-list-item-title>
+              <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn class="primary" :to="'/ad/' + order.adId">Open</v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -11,7 +27,15 @@
     name: 'OrderView',
     data() {
       return {
-
+        orders: [
+          {
+          	id:"123",
+          	name:"Kostya",
+          	phone:"+7(978)000-00-05",
+          	adId:"1",
+          	done:true
+          }
+	      ]
       }
     }
   }
