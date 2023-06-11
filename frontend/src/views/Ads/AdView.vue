a7e8d7e869c139c597924e6677f4bcb8a1cda683
LeoonWi <redwind090@gmail.com>
2023-06-12 02:17:50 +0300

    Верстка подробного просмотра

--- frontend/src/views/Ads/AdView.vue
+++ frontend/src/views/Ads/AdView.vue
@@ -1,18 +1,42 @@
 <template>
-    <v-container>
-      <v-row justify="center">
-            <h1>AdView</h1>
-      </v-row>
-    </v-container>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12>
+        <v-card class="mt-5">
+          <h1>Ad</h1>
+          <v-img
+            height="300px"
+            src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+          ></v-img>
+          <v-card-text
+            ><h1 class="text--primary mb-3">Lorem Ipsum</h1>
+            <p>
+              Lorem Ipsum has been the industry's standard dummy text ever since
+              the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book. It has survived not
+              only five centuries, but also the leap into electronic
+              typesetting, remaining essentially unchanged. It was popularised
+              in the 1960s with the release of Letraset sheets containing Lorem
+              Ipsum passages, and more recently with desktop publishing software
+              like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+          </v-card-text>
+          <v-card-actions
+            ><v-spacer></v-spacer>
+            <v-btn text class="warning">Edit</v-btn>
+            <v-btn class="success">Buy</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-  export default {
-    name: 'AdView',
-    data() {
-      return {
-
-      }
-    }
-  }
+export default {
+  name: 'AdView',
+  data() {
+    return {};
+  },
+};
 </script>
