6de8277d5ff9e72c34e78770277f44d4c8fa26e2
LeoonWi <redwind090@gmail.com>
2023-06-16 13:26:16 +0300

    Просмотр деталей объявлений

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -16,6 +16,7 @@ const routes = [
   {
     path: '/ad/:id',
     name: 'AdView',
+    props: true,
     component: AdView
   },
   {
--- frontend/src/views/Ads/AdView.vue
+++ frontend/src/views/Ads/AdView.vue
@@ -9,17 +9,8 @@
             src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
           ></v-img>
           <v-card-text
-            ><h1 class="text--primary mb-3">Lorem Ipsum</h1>
-            <p>
-              Lorem Ipsum has been the industry's standard dummy text ever since
-              the 1500s, when an unknown printer took a galley of type and
-              scrambled it to make a type specimen book. It has survived not
-              only five centuries, but also the leap into electronic
-              typesetting, remaining essentially unchanged. It was popularised
-              in the 1960s with the release of Letraset sheets containing Lorem
-              Ipsum passages, and more recently with desktop publishing software
-              like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
+            ><h1 class="text--primary mb-3">{{ ad.title }}</h1>
+            <p>{{ ad.desc }}</p>
           </v-card-text>
           <v-card-actions
             ><v-spacer></v-spacer>
@@ -35,8 +26,17 @@
 <script>
 export default {
   name: 'AdView',
+  props: ['id'],
   data() {
-    return {};
+    return {
+
+    }
   },
+  computed: {
+    ad() {
+      const id = this.id;
+      return this.$store.getters.adById(id);
+    }
+  }
 };
 </script>
